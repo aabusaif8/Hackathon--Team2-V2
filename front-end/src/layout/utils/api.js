f6adf9baaa81875b9c6f1dce989ffb8da894e777
TeddyLondon17 <alexanderstrife17@gmail.com>
2024-03-09 11:39:32 -0600

    updating links

--- front-end/src/layout/utils/api.js
+++ front-end/src/layout/utils/api.js
@@ -6,7 +6,7 @@ import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
+  process.env.REACT_APP_API_BASE_URL || "https://thinkfulrestaurantfinalproject-backend.onrender.com";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
