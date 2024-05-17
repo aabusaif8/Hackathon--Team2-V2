33258a07e1ed98f248ae4a77c388b5d78bd945d7
Matt Kulka <92747257+MattKulka@users.noreply.github.com>
2024-05-17 15:15:17 -0400

    initial navbar component created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,46 @@
 import React from 'react'
 
-function Navbar() {
-  return (
-    <div>
-      Site Name
-      <br />
-      <button>Log In/Sign Up</button>
-      <button>Investment Assistant</button>
-      <button>Manual Investment</button>
-      <button>Dashboard Overview</button>
-      <button>Learning Resources</button>
-    </div>
-  )
-}
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-export default Navbar
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <a href="/">Monarch</a>
+      </div>
+      <div className={`navbar-menu ${isOpen ? 'open' : ''}`}>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">Log In/Sign Up</a>
+          </li>
+          <li>
+            <a href="/services">Investment Assistant</a>
+          </li>
+          <li>
+            <a href="/contact">Manual Investment</a>
+          </li>
+          <li>
+            <a href="/contact">Dashboard Overview</a>
+          </li>
+          <li>
+            <a href="/contact">Learning Resources</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-toggle" onClick={toggleMenu}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
