d63c22988a5dc6fccae7769231d5a3e319180fb9
Francisco <francisco.alvarado217@yahoo.com>
2024-05-18 20:39:00 -0500

    Created articles for Learning Resource page and added routes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Dashboard from './pages/Dashboard';
 import InvestmentAssistant from './pages/InvestmentAssistant';
 import ManualInvestment from './pages/ManualInvestment';
 import LearningResources from './pages/LearningResources';
+import Article1 from './pages/Article1';
+import Article2 from './pages/Article2';
+import Article3 from './pages/Article3';
 
 
 function App() {
@@ -17,6 +20,9 @@ function App() {
           <Route path="/investment-assistant" element={<InvestmentAssistant />}/>
           <Route path="/manual-investment" element={<ManualInvestment />}/>
           <Route path="/learning-resources" element={<LearningResources />}/>
+          <Route path="/learning-resources-article1" element={<Article1 />} />
+          <Route path="/learning-resources-article2" element={<Article2 />} />
+          <Route path="/learning-resources-article3" element={<Article3 />} /> 
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/LearningResources.js
+++ src/pages/LearningResources.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Pic1 from "../images/rafiki.png"
+import Pic2 from "../images/phone-wallet.png"
+import Pic3 from "../images/money-pile.png"
+
 
 function LearningResources() {
   return (
@@ -9,9 +14,32 @@ function LearningResources() {
         <p className='text-lg mt-8'>Browse our collection of curated investment resources</p>
       </div>
 
-      <div className=''>
-        <input type="search" className='form-input mt-12 rounded-full w-64 bg-red-orange border-none drop-shadow-md'/>
-      </div>
+      <Link to="/learning-resources-article1">
+        <div className='sm:box-content h-60 bg-light-green mx-10 rounded-xl mt-10 pt-5 shadow-xl'>
+          <img src={Pic1} className='mx-10 '/>
+        </div>
+        <div className='sm:box-content h-36 mx-10 rounded-xl shadow-xl'>
+          <h1 className='text-center text-3xl pt-5'>Investing 101: How You Can Make the Most of Your Money</h1>
+        </div>
+      </Link>
+
+      <Link to="/learning-resources-article2">
+        <div className='sm:box-content h-60 bg-light-green mx-10 rounded-xl mt-20 pt-5 shadow-xl'>
+          <img src={Pic2} className='mx-10 '/>
+        </div>
+        <div className='sm:box-content h-36 mx-10 rounded-xl shadow-xl'>
+          <h1 className='text-center text-3xl pt-10'>Mobile Banking: The Pros and Cons</h1>
+        </div>
+      </Link>
+
+      <Link to="/learning-resources-article3">
+        <div className='sm:box-content h-60 bg-light-green mx-10 rounded-xl mt-20 pt-5 shadow-xl'>
+          <img src={Pic3} className='mx-10 '/>
+        </div>
+        <div className='sm:box-content h-36 mx-10 rounded-xl shadow-xl'>
+          <h1 className='text-center text-3xl pt-10'>5 Tips to Double your Savings in One Year</h1>
+        </div>
+      </Link>
     </div>
   )
 }
