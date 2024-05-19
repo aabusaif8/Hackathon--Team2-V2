e913cfe5190a0cc4909cae67467bc9c510f4adef
TeddyLondon17 <alexanderstrife17@gmail.com>
2024-05-19 10:14:17 -0500

    should be fully functional back end aside from date filter

--- Boilerplate-Backend/src/app.js
+++ Boilerplate-Backend/src/app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-const router = require('./back-end/router')
-const { chatCompletion } = require('./chatbot/chatbot-controller');
+// const router = require('./back-end/router')
+// const { chatCompletion } = require('./chatbot/chatbot-controller');
 
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
@@ -11,19 +11,16 @@ const notFound = require("./errors/notFound");
 const assetsRouter = require("./back-end/AssetFiles/AssetsRouter");
 const userInfoRouter = require("./back-end/UserInfo/UserInfoRouter"); // Import the UserInfoRouter
 
-const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Mount the assetsRouter under a specific path
 app.use("/assets", assetsRouter);
 
-// Mount the userInfoRouter under a specific path
 app.use("/users", userInfoRouter);
 
-app.use("/", router)
-app.post("/api/chatbot", chatCompletion);
+// app.use("/", router)
+// app.post("/api/chatbot", chatCompletion);
 
 
 app.use(notFound);
--- Boilerplate-Backend/src/chatbot/chatbot-controller.js
+++ Boilerplate-Backend/src/chatbot/chatbot-controller.js
@@ -1,27 +1,27 @@
-const { openai } = require("./config")
+// const { openai } = require("./config")
 
-const chatCompletion = async (req, res) => {
+// const chatCompletion = async (req, res) => {
 
-  try {
-    const completion = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    messages: [
-        {
-            role: "user", 
-            content: "Tell me 2 things about Kobe Bryant." // Replace with prompt from frontend chatbot form submit button (onClick event?)
-        }
-    ],
-    max_tokens: 100 // Determine token count size
-});
-    // console.log(completion.choices[0].message)
-    res.status(200).json({
-      completion: completion.choices[0].message
-    })
-  } catch (error) {
-    console.error('Error processing message:', error.message);
-    throw new Error('Failed to process message');
-  }
-  }
+//   try {
+//     const completion = await openai.chat.completions.create({
+//     model: "gpt-3.5-turbo",
+//     messages: [
+//         {
+//             role: "user", 
+//             content: "Tell me 2 things about Kobe Bryant." // Replace with prompt from frontend chatbot form submit button (onClick event?)
+//         }
+//     ],
+//     max_tokens: 100 // Determine token count size
+// });
+//     // console.log(completion.choices[0].message)
+//     res.status(200).json({
+//       completion: completion.choices[0].message
+//     })
+//   } catch (error) {
+//     console.error('Error processing message:', error.message);
+//     throw new Error('Failed to process message');
+//   }
+//   }
 
 
-module.exports = { chatCompletion }
+// module.exports = { chatCompletion }
