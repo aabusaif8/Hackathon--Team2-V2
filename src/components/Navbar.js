f6d3cf413f60e19dcf9dee3b01ba8d4f8d94bb9c
unknown <vrouzbeh@yahoo.com>
2024-05-20 02:12:02 -0700

    fixing issues with code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,54 +1,42 @@
-
-import React, { useState, Fragment } from "react";
-import { Link } from "react-router-dom";
-import logo from "../assets/logo.png";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import { Dialog, Disclosure, Popover, Transition } from "@headlessui/react";
-
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../assets/logo.png'
+import {Fragment } from "react";
+import {Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline"; 
+import {Dialog, Disclosure, Popover, Transition } from "@headlessui/react"
 
 function Navbar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
     <header className="bg-[#97db51]">
-      <nav
-        className="mx-auto flex max-w-7xl items-center justify between p-6 lg:px-8"
-        aria-label="Global"
-      >
-        <div className="flex lg:flex-1">
-          <a href="/" className="-m-1.5 p-1.5">
-            <span className="sr-only">Monarch.com</span>
-            <img href="../" className="h-14 w-auto" src={logo} alt="logo" />
-          </a>
-        </div>
-        <div className="flex lg:hidden justify-end">
-          <button
-            type="button"
-            className="-m-2.5 inline-flex justify-end rounded-md p-2.5 text-white "
-            onClick={() => setMobileMenuOpen(true)}
+    <nav className="mx-auto flex max-w-7xl items-center justify between p-6 lg:px-8"
+         aria-label="Global"
+        >
+          <div className="flex lg:flex-1">
+            <a href="/" className="-m-1.5 p-1.5">
+                <span className="sr-only">Monarch.com</span>
+                <img
+                  href="../"
+                  className="h-14 w-auto"
+                  src={logo}
+                  alt="logo"
+                  />
+            </a>
+          </div>
+          <div className="flex lg:hidden justify-end">
+            <button 
+              type="button"
+              className="-m-2.5 inline-flex justify-end rounded-md p-2.5 text-white "
+              onClick={() => setMobileMenuOpen(true)}
           >
-            <span className="sr-only">Open main menu</span>
-            <Bars3Icon
-              className="h-6 w-6 flex justify-end"
-              aria-hidden="true"
-            />
-          </button>
-        </div>
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon className="h-6 w-6 flex justify-end" aria-hidden="true" />
+            </button>
+          </div>
 
-        <Popover.Group className="hidden lg:flex lg:gap-x-12">
-          <a href="#" className="text-sm font-semibold leading-6 text-white">
-            Investment Assistant
-          </a>
-          <a href="#" className="text-sm font-semibold leading-6 text-white">
-            Manual Investment
-          </a>
-          <a href="#" className="text-sm font-semibold leading-6 text-white">
-            Dashboard Overview
-          </a>
-          <a href="#" className="text-sm font-semibold leading-6 text-white">
-            Learning Resources
-          </a>
-        </Popover.Group>
+          <Popover.Group className="hidden lg:flex lg:gap-x-12">
+
             <a href="/manual-investment" className="text-sm font-semibold leading-6 text-white">
                 Manual Investment
             </a>
@@ -64,14 +52,14 @@ function Navbar() {
           </Popover.Group>
 
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="#" className="text-sm font-semibold leading-6 text-white">
+            <Link to="/signin" className="text-sm font-semibold leading-6 text-white">
                 Sign In <span aria-hidden="true">&rarr;</span>
-            </a>
+            </Link>
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="#" className="text-sm font-semibold leading-6 text-white">
+            <Link to="/signup" className="text-sm font-semibold leading-6 text-white">
                 Sign Up <span aria-hidden="true">&rarr;</span>
-            </a>
+            </Link>
           </div>
         </nav>
         <Dialog
@@ -79,28 +67,30 @@ function Navbar() {
         className="lg:hidden"
         open={mobileMenuOpen}
         onClose={setMobileMenuOpen}
-      >
+        >
         <div className="fixed inset-0 z-10" />
 
-        <Dialog.Panel
-          className="fixed inset-y-0 right-0 z-10 w-full 
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full 
         overflow-y-auto bg-[#97db51] px-6 py-6 sm:max-w-sm sm:ring-1 
-        sm:ring-gray-900/10"
-        >
-          <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Monarch</span>
-              <img className="h-8 w-auto" src={logo} alt="" />
-            </a>
-            <button
-              type="button"
-              className="-m-2.5 rounded-md p-2.5 text-white"
-              onClick={() => setMobileMenuOpen(false)}
-            >
-              <span className="sr-only">Close Menu</span>
-              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-            </button>
-          </div>
+        sm:ring-gray-900/10">
+            <div className="flex items-center justify-between">
+                <a href="#" className="-m-1.5 p-1.5">
+                  <span className="sr-only">Monarch</span>
+                  <img
+                  className="h-8 w-auto"
+                  src={logo}
+                  alt=""
+                />
+                </a>
+                <button
+                  type="button"
+                  className="-m-2.5 rounded-md p-2.5 text-white"
+                  onClick={() => setMobileMenuOpen(false)}
+                >
+                  <span className="sr-only">Close Menu</span>
+                  <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+            </div>
 
             <div className="mt-6 flow-root">
                 <div className="-my-6 divide-y divide-gray-500/10">
@@ -110,7 +100,6 @@ function Navbar() {
                         href="/manual-investment"
                         className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white 
                         hover:bg-[#97db51]"
-
                         >
                           Manual Investment
                         </a>
@@ -124,7 +113,6 @@ function Navbar() {
                         <a
                         href="/learning-resources"
                         className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white 
-
                         hover:bg-[#97db51]"
                         >
                           Learning Resources
@@ -140,31 +128,28 @@ function Navbar() {
         
                     </div>
                     <div className="py-6">
-                      <a 
-                      href="#"
+                      <Link to="/signin"
                       className="-mx-3 block rounded-lg px-2 py-2.5 text-base font-semibold leading-7 text-white
                       hover:bg-[#97db51]"
                       >
                         Sign In
-                      </a>
+                      </Link>
                     </div>
 
                     <div className="py-6">
-                      <a 
-                      href="#"
+                      <Link to="signup"
                       className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white
                       hover:bg-[#97db51]"
                       >
                         Sign Up
-                      </a>
+                      </Link>
                     </div>
                 </div>
             </div>
-          </div>
-        </Dialog.Panel>
-      </Dialog>
+          </Dialog.Panel>
+        </Dialog>
     </header>
   );
 }
-
+    
 export default Navbar;
