10c99d5884703e4f937b82ceaa7043754c6c6df9
Cory <cmorgan3324@gmail.com>
2024-05-19 21:11:02 -0400

    Updated chatbot controller

--- Boilerplate-Backend/src/chatbot/chatbot-controller.js
+++ Boilerplate-Backend/src/chatbot/chatbot-controller.js
@@ -1,19 +1,24 @@
 const { openai } = require("./config")
 
 const chatCompletion = async (req, res) => {
-
+  const { prompt } = req.body;
   try {
     const completion = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
-    messages: [
-        {
-            role: "user", 
-            content: "Tell me 2 things about Kobe Bryant." // Replace with prompt from frontend chatbot form submit button (onClick event?)
-        }
-    ],
-    max_tokens: 100 // Determine token count size
+    prompt: prompt,
+    temperature: 0,
+    max_tokens: 500,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    // messages: [
+    //     {
+    //         role: "user", 
+    //         content: "What are 3 stocks that I should invest in for a long term gain?" // Replace with prompt from frontend chatbot form submit button (onClick event?)
+    //     }
+    // ],
 });
-    // console.log(completion.choices[0].message)
+    console.log(completion.choices[0].message)
     res.status(200).json({
       completion: completion.choices[0].message
     })
