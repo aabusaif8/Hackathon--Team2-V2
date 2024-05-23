808acd2342984e04414d5faba825245a79471317
Rouzbeh Vahdatiasl <137455435+Rouz275@users.noreply.github.com>
2024-05-22 20:21:36 -0700

    Update AccountFeatures.js

--- src/components/AccountFeatures.js
+++ src/components/AccountFeatures.js
@@ -1,60 +1,37 @@
 import React from "react";
-import dashboard from "../assets/dashboard.png";
-import security from "../assets/security.png";
-import accounttype from "../assets/accounttype.png";
+import { Link } from "react-router-dom";
+import analysis from "../assets/analysis.png";
 
-const AccountFeatures = () => {
+const Analysis = () => {
   return (
-    <div className="bg-[#C5eb9e] py-16 md:py-24 lg:py-32">
+    <div className="bg-red-orange py-16 md:py-24 lg:py-32">
       <div className="max-w-7xl mx-auto px-4">
         <div className="text-center mb-12">
           <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold">
-            Have full transparency and control over your portfolio
+            Already an investment pro? Take control of your portfolio with stock
+            analysis and projections!
           </h1>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 ">
-          <div className="bg-white rounded-lg shadow-lg p-4 md:p-6 flex flex-col items-center">
-            <h2 className="text-lg md:text-xl lg:text-2xl font-semibold mt-4 mb-2">
-              Personal Investment Dashboard
-            </h2>
-            <p className="text-gray-600 text-center mt-4">
-              Get a comprehensive view of your portfolio with real-time updates and detailed analytics.
-            </p>
+        <div className="flex flex-col items-center">
+          <div className="mb-8">
             <img
-              src={dashboard}
-              alt="Dashboard"
-              className="mx-auto max-w-full h-auto"
+              src={analysis}
+              alt="Analysis"
+              className="mx-auto max-w-full h-64"
             />
-            
           </div>
-          <div className="bg-white rounded-lg shadow-lg p-4 md:p-6">
-            <div className="flex flex-col items-center mb-8">
-        
-              <h2 className="text-lg md:text-xl lg:text-2xl font-semibold mt-4 mb-2">
-                Manage Multiple Accounts
-              </h2>
-              <p className="text-gray-600 text-center">
-                Consolidate your savings, retirement, and loan repayment accounts in one place.
-              </p>
-            </div>
-            <div className="flex flex-col items-center">
-              <img
-                src={accounttype}
-                alt="Account Type"
-                className="mx-auto max-w-full h-auto"
-              />
-              <h2 className="text-lg md:text-xl lg:text-2xl font-semibold mt-4 mb-2 pt-5 ">
-                Secure and Accessible
-              </h2>
-              <p className="text-gray-600 text-center pb-5 ">
-                Enjoy peace of mind with robust security measures and easy access to your information.
-              </p>
-              <img
-                src={security}
-                alt="security"
-                className="mx-auto max-w-full h-auto"
-              />
-            </div>
+          <div className="mb-8">
+            <p className="text-lg md:text-xl lg:text-2xl font-semibold text-center">
+              Explore a stock's historical performance and view our projections
+              to make informed investment decisions in real-time.
+            </p>
+          </div>
+          <div className="flex justify-center">
+            <Link to="/signpage">
+              <button className="bg-dark-green text-white py-3 px-8 md:px-12 lg:px-16 rounded-full font-semibold transition-colors duration-300 hover:bg-green-700">
+                Open an Account
+              </button>
+            </Link>
           </div>
         </div>
       </div>
@@ -62,4 +39,4 @@ const AccountFeatures = () => {
   );
 };
 
-export default AccountFeatures;
+export default Analysis;
