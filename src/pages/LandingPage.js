b0d841ea40bd11830df2a3cb8d8942670e81ce08
unknown <vrouzbeh@yahoo.com>
2024-05-23 01:02:33 -0700

    landing page redirect

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { useNavigate, useParams } from "react-router-dom";
 import NavBar from '../components/Navbar';
 import Hero from '../components/Hero';
 import DigitalAssistant from '../components/DigitalAssistant';
@@ -7,8 +7,19 @@ import AccountFeatures from '../components/AccountFeatures';
 import Analysis from '../components/Analysis';
 import Learn from '../components/Learn';
 import Footer from '../components/Footer';
+import { useAuth } from "../context/AuthContext";
 
 const LandingPage = () => {
+  const { isLoggedIn } = useAuth();
+  const { userId } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate(`/${userId}/dashboard`, { replace: true });
+    }
+  }, [isLoggedIn, navigate, userId]);
+  
   return (
     <div>
 
