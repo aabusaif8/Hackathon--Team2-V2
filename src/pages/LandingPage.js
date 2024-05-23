0db5f799f91928a4295530116e8f5fd53a726549
Ahmad Abusaif <129558794+aabusaif8@users.noreply.github.com>
2024-05-23 10:09:54 -0500

    Update LandingPage.js
    imported useEffect

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import {React, useEffect} from 'react';
 import { useNavigate, useParams } from "react-router-dom";
 import NavBar from '../components/Navbar';
 import Hero from '../components/Hero';
